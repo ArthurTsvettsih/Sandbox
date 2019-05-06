d9f47967cc70b49c9c7015660d2c6c58802d02c7
Arthur Tsvettsih <arthur.tsvettsih@gmail.com>
2019-05-06 20:19:40 +0100

    Created Basic StringManipulator

---
+++ StringManipulator/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import StringManipulator from './components/stringManipulator'
+
+function App() {
+  return (
+    <div className="App">
+      <StringManipulator />
+    </div>
+  );
+}
+
+export default App;
