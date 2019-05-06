3e097d7adbf27a308fece97ffe8714108b4f35e1
Arthur Tsvettsih <arthur.tsvettsih@gmail.com>
2019-05-06 19:22:26 +0100

    Added AutoChessExpCalculator and Sandbox

---
+++ AutoChessExpCalculator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoChessExpCalculator
+{
+    /// <summary>
+    /// Description: Calculator for calculating "Dota 2 AutoChess" level up cost
+    /// Tech: .Net 4.6.2 Console App. Currently it is difficult to generate standalone executables in .Net Core
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                Console.WriteLine("Current XP:");
+                var currentXp = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Target XP:");
+                var targetXp = float.Parse(Console.ReadLine());
+
+                var requiredGold = Math.Ceiling((targetXp - currentXp) / 4) * 5;
+                var waitTurns = (targetXp - currentXp) % 4;
+
+                Console.WriteLine();
+                Console.WriteLine("-------");
+                Console.WriteLine($"You need to spend: {requiredGold} gold");
+
+                if (waitTurns != 0)
+                {
+                    var turns = waitTurns == 1 ? "turn" : "turns";
+                    Console.WriteLine($"Or wait {waitTurns} {turns} and pay {requiredGold - 5} gold");
+                }
+                
+                Console.WriteLine("-------");
+
+                Console.WriteLine();
+                Console.WriteLine("Press any key to clear the console...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ Sandbox/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Sandbox
+{
+    /// <summary>
+    /// Description: Placeholder project for working on temporary stuff that doesn't deserve a standalone project
+    /// Tech: .Net Core Console App
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
